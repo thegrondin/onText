0e8235e454167834388d58e8e0711449a8639e33
thegrondin <tomtom9461@gmail.com>
2020-03-18 23:51:16 -0400

    Adding textarea controls for edition

--- renderer.js
+++ renderer.js
@@ -1,9 +1,13 @@
-var showdown  = require('showdown'),
-converter = new showdown.Converter();
+const showdown  = require('showdown');
+const autosize = require('autosize');
+
+let converter = new showdown.Converter();
 
 const Editor = () => {
-    let addEvent = (element) => {
-        element.addEventListener('click', (e) => {
+
+    const makeEditable = (from, to = null) => {
+
+        /*const edit = (element) => {
 
             const wrap = document.createElement('div');
            
@@ -18,24 +22,95 @@ const Editor = () => {
             editor.insertBefore(inputElement, element);
 
             element.remove();
+        }*/
 
+        const edit = (from, to) => {
+            
+            if (!from || !to) return;
+
+            let next = from;
+            let html = "";
+
+            let elements = [];
+
+            while(next != to && next && next.type != 'textarea' ) {
+    
+                elements.push(next);
+
+                console.log("Next", next);
+                const wrap = document.createElement('div');
+                wrap.appendChild(next.cloneNode(true));
+
+                html += wrap.innerHTML;
+                next = next.nextSibling.nextElementSibling;
+                
+                wrap.remove();
+                
+            }
+
+            const wrap = document.createElement('div');
+            wrap.appendChild(to.cloneNode(true));
+
+            html += wrap.innerHTML;
+
+            let editSection = document.createElement('textarea');
+            editSection.value = converter.makeMarkdown(html);
+            editSection.style.width = "100%"
+            
+
+            editor.insertBefore(editSection, to);
+
+            autosize(document.querySelectorAll('textarea'));
+
+            elements.push(to);
+            console.log("To", to);
+
+            for (let element of elements) {
+                element.remove();
+            }
+        }
+
+        if (!to) {
+            edit(from, from);
+            return;
+        }
+        
+        edit(from, to);
+        
+    }
+
+    let addEvent = (element) => {
+        element.addEventListener('click', (e) => {
+            makeEditable(element);
         })
     }
 
     const editor = document.querySelector('.editor');
     
-
     window.addEventListener('keypress', (e) => {
 
         if (e.key == "Enter" && e.shiftKey) {
+            console.log('test');
             for (let element of Array.from(editor.childNodes)) {
 
-                if (element instanceof HTMLInputElement) {
+                if (element instanceof HTMLTextAreaElement) {
                     const html = converter.makeHtml(element.value);
-                    const htmlElement = new DOMParser().parseFromString(html, 'text/html').body.childNodes[0];
+                    const nodes = new DOMParser().parseFromString(html, 'text/html').body.childNodes;
+                   // console.log(htmlElement);
 
-                    editor.insertBefore(htmlElement, element);
-                    addEvent(htmlElement);
+                 
+                    for (let node of nodes) {
+
+                        
+                         
+                            editor.insertBefore(node, element);
+                            addEvent(node);
+
+                        
+                    }
+
+                    //editor.insertBefore(htmlElement, element);
+                    
 
                     element.remove();
     
@@ -43,9 +118,27 @@ const Editor = () => {
     
             }
         }
+    });
 
-        
+    window.addEventListener('mouseup', () => {
+        const selection = window.getSelection();
 
+        if (selection.type == "Range") {
+            const baseNode = selection.baseNode.parentNode;
+            const extendNode = selection.extentNode.parentNode;
+
+            const next = extendNode.nextSibling.nextElementSibling
+           
+            console.log(baseNode, extendNode, next, selection);
+
+            if (Array.from(editor.childNodes).indexOf(baseNode) > Array.from(editor.childNodes).indexOf(extendNode)) {
+                makeEditable(extendNode, baseNode);
+            }
+            else {
+                makeEditable(baseNode, extendNode);
+            }
+
+        }
     });
 
     const editorElements = editor.childNodes;
@@ -53,8 +146,6 @@ const Editor = () => {
     for (let element of Array.from(editorElements)) {
         addEvent(element);
     }
-
-
 }
 
 Editor();
